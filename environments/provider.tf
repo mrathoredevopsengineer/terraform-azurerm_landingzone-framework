59dde6e44c982c4a3069e2d21da0ad598ce52612
Mahesh Kumar Rathore <m.rathore176@gmail.com>
2025-11-24 15:38:21 +0530

    subcription_id removed from provider.tf

--- environments/provider.tf
+++ environments/provider.tf
@@ -1,5 +1,5 @@
 terraform {
- # backend "azurerm" {}
+  backend "azurerm" {}
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -10,5 +10,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "6dbc33a2-5da4-4090-8ac2-b8dde7d2a834"
 }
